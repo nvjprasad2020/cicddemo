2554546f186c6abf9cc3c9e2430530c8a07e8918
jayaprasad <nvjprasad2020@gmail.com>
2024-10-25 09:14:31 +0100

    change 4

--- src/main/java/com/example/demo/data/EmployeesDB.java
+++ src/main/java/com/example/demo/data/EmployeesDB.java
@@ -9,11 +9,11 @@ public class EmployeesDB {
     private static List<Employee> employeeList = new ArrayList<>();
 
     static {
-        employeeList.add(new Employee(1, "John Doe", "Software Engineer", 75000.0f));
-        employeeList.add(new Employee(2, "Jane Smith", "Project Manager", 85000.0f));
-        employeeList.add(new Employee(3, "Michael Brown", "DevOps Engineer", 70000.0f));
-        employeeList.add(new Employee(4, "Sarah Johnson", "Business Analyst", 65000.0f));
-        employeeList.add(new Employee(5, "Chris Evans", "QA Engineer", 60000.0f));
+        employeeList.add(new Employee(1, "jp", "Software Engineer", 75000.0f));
+        employeeList.add(new Employee(2, "siva", "Project Manager", 85000.0f));
+        employeeList.add(new Employee(3, "kiran", "DevOps Engineer", 70000.0f));
+        employeeList.add(new Employee(4, "pavan", "Business Analyst", 65000.0f));
+        employeeList.add(new Employee(5, "prasad", "QA Engineer", 60000.0f));
     }
 
     public static List<Employee> getAllEmployees() {
