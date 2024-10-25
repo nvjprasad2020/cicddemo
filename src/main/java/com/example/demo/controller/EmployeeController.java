a396cffb43c2bc8816579ce2b204c7313def742a
jayaprasad <nvjprasad2020@gmail.com>
2024-10-25 02:15:29 +0100

    a change

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,4 +23,11 @@ public class EmployeeController {
         logger.info("Getting all employees ");
         return ResponseEntity.ok(EmployeesDB.getAllEmployees());
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<Employee>> getAllEmployees(@PathVariable String id) {
+        logger.info("Getting all employees ");
+        return ResponseEntity.ok(EmployeesDB.getAllEmployees());
+    }
+
 }
--- src/main/java/com/example/demo/data/EmployeesDB.java
+++ src/main/java/com/example/demo/data/EmployeesDB.java
@@ -7,14 +7,15 @@ import java.util.List;
 
 public class EmployeesDB {
     private static List<Employee> employeeList = new ArrayList<>();
-
-    public static List<Employee> getAllEmployees() {
-         // Add 5 Employee records to the list
+    {
         employeeList.add(new Employee(1, "John Doe", "Software Engineer", 75000.0f));
         employeeList.add(new Employee(2, "Jane Smith", "Project Manager", 85000.0f));
         employeeList.add(new Employee(3, "Michael Brown", "DevOps Engineer", 70000.0f));
         employeeList.add(new Employee(4, "Sarah Johnson", "Business Analyst", 65000.0f));
         employeeList.add(new Employee(5, "Chris Evans", "QA Engineer", 60000.0f));
+    }
+
+    public static List<Employee> getAllEmployees() {
         return employeeList;
     }
 }
